cee0eabfb1aa9fb0407f2d9c6d51f39f1a189887
Rob <robert.thwaite@hotmail.co.uk>
2017-01-09 17:39:22 +0000

    Initial Copy

---
+++ index.js
@@ -0,0 +1,17 @@
+"use strict";
+// core
+var imageCropperModule_1 = require('./src/imageCropperModule');
+exports.ImageCropperModule = imageCropperModule_1.ImageCropperModule;
+var imageCropperComponent_1 = require('./src/imageCropperComponent');
+exports.ImageCropperComponent = imageCropperComponent_1.ImageCropperComponent;
+// extra classes
+var imageCropper_1 = require('./src/imageCropper');
+exports.ImageCropper = imageCropper_1.ImageCropper;
+var cropperSettings_1 = require('./src/cropperSettings');
+exports.CropperSettings = cropperSettings_1.CropperSettings;
+var cropperDrawSettings_1 = require('./src/cropperDrawSettings');
+exports.CropperDrawSettings = cropperDrawSettings_1.CropperDrawSettings;
+// models
+var bounds_1 = require('./src/model/bounds');
+exports.Bounds = bounds_1.Bounds;
+//# sourceMappingURL=index.js.map
